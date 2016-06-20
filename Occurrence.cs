a7d4743759f540ed687eef81584e7e5077f9ae69
ohj35 <justinoh2010@gmail.com>
2016-06-20 16:48:10 -0700

    newstuff

--- Occurrence.cs
+++ Occurrence.cs
@@ -8,33 +8,33 @@ namespace PhotoTagger
 {
     public struct word_freq
     {
-        public string word;
-        public int freq;
+        public string word;//string that stores the occurrence word
+        public int freq;//number of times a word occurs in document(to be translated into weight in later processing)
         public word_freq(string wrd)
         {
             word = wrd;
-            freq = 1;
+            freq = 1;//init to 1 as the first occurrence causes construction
         }
 
-    };
+    };//word_freq
 
     public class Occurrence
     {
-        public static List<word_freq> freq_check(string document)
+        public static List<word_freq> freq_check(string document)//params: document-the string of the entire document to be processed
         {
-            document = document.ToLower();
-            document = document.Trim();
+            document = document.ToLower();//don't compare case
+            document = document.Trim();//remove leading and trailing whitespace
             for(int i = 0; i < document.Length;i++)
             {
-                if(Char.IsPunctuation(document, i) && document.ToCharArray()[i] != '\'')
+                if (Char.IsPunctuation(document, i) && document.ToCharArray()[i] != '\'')//find non ' punctuation
                 {
-                    document = document.Replace(document.ToCharArray()[i], ' ');
-                    i--;
+                    document = document.Replace(document.ToCharArray()[i], ' ');//remove all occurrences of non ' punctuation
+                    i--;//don't get ob1
                 }
             }
             while(document.IndexOf("  ") != -1)
             {
-                document = document.Replace("  ", " ");
+                document = document.Replace("  ", " ");//replace double space with single space
             }
             List<word_freq> freq_list = new List<word_freq>();
             while (document != "")
@@ -42,25 +42,25 @@ namespace PhotoTagger
                 string checking;
                 int inde;
                 if (document.IndexOf(" ") >= 0)
-                    inde = document.IndexOf(" ");
+                    inde = document.IndexOf(" ");//find the end of the first word
                 else
-                    inde = document.Length;
-                checking = document.Substring(0, inde);
-                document = document.Substring(inde);
-                word_freq to_make = new word_freq(checking);
+                    inde = document.Length;//unless it is the last word
+                checking = document.Substring(0, inde);//get the first word
+                document = document.Substring(inde);//remove the first word from document
+                word_freq to_make = new word_freq(checking);//construct a new word_freq struct
                 //Console.Write(checking + " ");
-                while (document.IndexOf(" " + checking + " ") != -1)
+                while (document.IndexOf(" " + checking + " ") != -1)//check if the word is in the string. Spaces at beginning and end so that letters are not pulled from full words
                 {
-                    int index = document.IndexOf(" " + checking + " ");
-                    document = document.Substring(0, index) + document.Substring(index + checking.Length + 1);
-                    to_make.freq++;
+                    int index = document.IndexOf(" " + checking + " ");//Get index of single word. Spaces at beginning and end so that letters are not pulled from full words
+                    document = document.Substring(0, index) + document.Substring(index + checking.Length + 1);//remove word from document string
+                    to_make.freq++;//increment the frequency of current word
                 }
                 
-                freq_list.Add(to_make);
-                document = document.Trim();
+                freq_list.Add(to_make);//add to list
+                document = document.Trim();//remove whitespace to prevent infinite looping
             }
-            var new_list = freq_list.OrderBy(x => -x.freq);
-            return new_list.ToList<word_freq>();
+            var new_list = freq_list.OrderBy(x => -x.freq);//lambda has a negative to sort by nonincreasing order
+            return new_list.ToList<word_freq>();//return sorted list
         }
-    }
+    }//freq_check(string document)
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using HtmlAgilityPack;
 
 namespace PhotoTagger
@@ -12,19 +13,51 @@ namespace PhotoTagger
         static void Main(string[] args)
         {
             HtmlWeb web = new HtmlWeb();
-            HtmlDocument document = web.Load("http://www.techtimes.com/articles/165916/20160620/oneplus-3-vs-google-nexus-5x-which-midrange-smartphone-should-you-buy.htm");
-            HtmlNode[] nodes = document.DocumentNode.SelectNodes("//p").ToArray();
+            HtmlDocument document = web.Load("http://www.pcmag.com/news/345423/teslas-model-s-will-sort-of-swim");//Loads the webpage, change string to change webpage
+            HtmlNode[] nodes = document.DocumentNode.SelectNodes("//p").ToArray();//gets all document lines in the <p> tags
             String textDocument = "";
             foreach (HtmlNode item in nodes)
             {
-                textDocument += item.InnerText;
+                textDocument += item.InnerText;//builds a string version of the text document
             }
-            List<word_freq> wordList = Occurrence.freq_check(textDocument);
+            List<word_freq> wordList = Occurrence.freq_check(textDocument);//calls a frequency check (see Occurrences.cs)
+            /*RemoveWords(wordList, "pronouns.txt");
+            RemoveWords(wordList, "prepositions.txt");
+            RemoveWords(wordList, "titles.txt");
+            RemoveWords(wordList, "verbs.txt");
+            RemoveWords(wordList, "conjunctions.txt");
+            RemoveWords(wordList, "commonwords.txt");*/
+            RemoveWords(wordList, "stopwords.txt");//removes occurrences of words from list based on a txt document of stopwords(see stopwords.txt)
             foreach(word_freq word in wordList)
             {
-                Console.WriteLine(word.word + ": " + word.freq);
+                Console.WriteLine(word.word + ": " + word.freq);//Debug test print
             }
-            Console.ReadLine();
+            Console.ReadLine();//keep console open
         }
+        
+        private static void RemoveWords(List<word_freq> wordList, String filename)//params: wordList-List of occurrences to have removed, filename-file with words to be removed
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filename))
+                {
+                    do
+                    {
+                        String word = sr.ReadLine();//get word to remove from txt file
+                        for (int i = 0; i < wordList.Count; i++)
+                        {
+                            word_freq wordCombo = wordList[i];
+                            if (wordCombo.word.Equals(word.ToLower()))//don't compare case
+                                wordList.RemoveAt(i);//remove word
+                        }
+                    } while (!sr.EndOfStream);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }//RemoveWords(List<word_freq> wordList, String filename)
     }
 }
