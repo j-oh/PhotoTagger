ba2303b7d3790b7f246263532f250e606263a7bb
Aniranth <aniranth@gmail.com>
2016-06-23 12:56:08 -0700

    SentenceAnalysisv1
    Added the preliminary class SentenceAnalyzer and a txt document to help
    it discern key words in a sentence

---
+++ SentenceAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoTagger
+{
+    class SentenceAnalyzer
+    {
+        public static void AnalyzeDocument(List<word_weight> weightedList, string document)//params: weightedList-the list that has scores to be updated, document-string containing the document
+        {
+            //prepare document to be analyzed
+            document = document.ToLower();//don't compare case
+            document = document.Trim();//remove leading and trailing whitespace
+            for (int i = 0; i < document.Length; i++)
+            {
+                if (document.ToCharArray[i] == ' ')//remove all punctuation except end punctuation and apostrophes
+                {
+
+                }
+            }
+        }//AnalyzeDocument(List<word_weight> weightedList, string document)
+    }
+}
