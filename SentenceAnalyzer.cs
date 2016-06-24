dc6bec9eb4b298dbb55a5cf3b07e77503f1cd39a
Aniranth <aniranth@gmail.com>
2016-06-24 15:56:52 -0700

    SentenceAnalyzer
    Completed class with documentation on how to use it

--- SentenceAnalyzer.cs
+++ SentenceAnalyzer.cs
@@ -15,18 +15,18 @@ namespace PhotoTagger
 
         public static void AnalyzeDocument(List<word_weight> weightedList, string document)//params: weightedList-the list that has scores to be updated, document-string containing the document
         {
-            string to_process = document;
+            string to_process = document;//don't want to change the original doc
             //prepare document to be analyzed
             to_process = to_process.ToLower();//don't compare case
             to_process = to_process.Trim();//remove leading and trailing whitespace
-            var s = new StringBuilder();
+            var s = new StringBuilder();//trying to get punctuation removal to work nicely
             foreach (char sb in to_process)
             {
                 if (!char.IsPunctuation(sb) || punctuationToKeep.IndexOf(" "+ sb + " ") == 1)//remove all punctuation except end punctuation and apostrophes
                 {
-                    s.Append(sb);//remove all occurrences of non ' punctuation                   
+                    s.Append(sb);//adds the chars of the string to a string builder                 
                 }
-                if (sb == '.')
+                if (sb == '.')//I couldn't make a nice way to keep end punctuation so i just brute forced it. I added spaces after punctuation to help the writers of articles out a bit
                 {
                     s.Append(". ");
                 }
@@ -49,7 +49,7 @@ namespace PhotoTagger
                 else if (sb == ':')
                 {
                     s.Append(": ");
-                }
+                }//the punctuation included are ones that may delineate thoughts of an author
             }
             to_process = s.ToString();
             while (to_process.IndexOf("  ") != -1)
@@ -58,7 +58,7 @@ namespace PhotoTagger
             }
             //end prepare title content for processing
             string sentence = "";
-            int index = Int32.MaxValue;
+            int index = Int32.MaxValue;//set to max value to determine the lowest possible punctuation point in document
             while (to_process != "")
             {
                 index = Int32.MaxValue;
@@ -68,32 +68,32 @@ namespace PhotoTagger
                     {
                         if (to_process.IndexOf(punctuation[i]) != -1)
                         {
-                            index = to_process.IndexOf(punctuation[i]);
+                            index = to_process.IndexOf(punctuation[i]);//finds the first point of any punctuation in the document
                         }
                     }
                 }
                 if (index == Int32.MaxValue)
                 {
-                    index = to_process.Length - 1;
+                    index = to_process.Length - 1;//if it is the last punctuation just go to the end of the doc
                 }
-                sentence = to_process.Substring(0, index);
-                to_process = to_process.Substring(sentence.Length + 1);
+                sentence = to_process.Substring(0, index);//get the sentence to analyze
+                to_process = to_process.Substring(sentence.Length + 1);//remove sentence from process string
                 try
                 {
-                    using (StreamReader sr = new StreamReader("importancedelineators.txt"))
+                    using (StreamReader sr = new StreamReader("importancedelineators.txt"))//importancedelineators.txt stores all the leading words that could signify location, people, objects, etc
                     {
                         do
                         {
-                            String word = sr.ReadLine();//get word to remove from txt file
+                            String word = sr.ReadLine();//get key word from txt file
                             for (int i = 0; i < weightedList.Count; i++)
                             {
                                 word_weight weight = weightedList[i];
-                                int pos_tag = sentence.IndexOf(" " +weight.word+" ");
-                                int pos_key = sentence.IndexOf(" " + word + " ");
-                                if (pos_key != -1 && pos_tag != -1 && Math.Abs(pos_tag - pos_key) <= 15)//compare the distance from the key word and its subject
+                                int pos_tag = sentence.IndexOf(" " +weight.word+" ");//gets the index of a possible tag to compare to the words in the txt file
+                                int pos_key = sentence.IndexOf(" " + word + " ");//gets any index of a word that delineates importance
+                                if (pos_key != -1 && pos_tag != -1 && Math.Abs(pos_tag - pos_key) <= 15)//compare the distance from the key word and its subject. if either aren't in the sentence don't add points
                                 {
-                                    weight.points += 2;//add points
-                                    weightedList[i] = weight;
+                                    weight.points += 2;//add points if the delineator is close to a tag word
+                                    weightedList[i] = weight;//add the newly allocated points to the List
                                 }
                             }
                         } while (!sr.EndOfStream);
@@ -102,9 +102,9 @@ namespace PhotoTagger
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.Message);//catch file io exceptions
                 }
-                to_process = to_process.Trim();
+                to_process = to_process.Trim();//remove whitespace so the while loop will terminate
             }
         }//AnalyzeDocument(List<word_weight> weightedList, string document)
     }
